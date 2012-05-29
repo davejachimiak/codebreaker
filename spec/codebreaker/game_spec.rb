baad8f7a8739e138220b349ff755aecca787542b
Dave Jachimiak <dave.jachimiak@gmail.com>
2012-05-29 14:53:33 -0400

    initial commit

---
+++ spec/codebreaker/game_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+module Codebreaker
+  describe Game do
+    let(:output) { double('output').as_null_object }
+    let(:game)   { Game.new(output) }
+
+    describe "#start" do
+      it "sends a welcome message" do
+        output.should_receive(:puts).with('Welcome to Codebreaker!')
+        game.start('1234')
+      end
+
+      it "prompts for the first guess" do
+        output.should_receive(:puts).with('Enter guess:')
+        game.start('1234')
+      end
+    end
+
+    describe "#guess" do
+      context "valid guess" do
+        it "sends the mark to output" do
+          game.start('1234')
+	  output.should_receive(:puts).with('++++')
+	  game.guess('1234')
+        end
+      end
+
+      context "invalid guess" do
+        let(:message) { 'You must guess exactly four numbers' }
+	before(:each) do
+          game.start('1234')
+	  output.should_receive(:puts).with(message)
+        end
+
+	it "sends invalid message for less than four" do
+	  game.guess('123')
+        end
+
+        it "sends invalid message for more than four numbers" do
+	  game.guess('12345')
+        end
+	it "sends invalid message for any non-digit character" do
+	  game.guess('1b3b')
+        end
+      end
+    end
+  end
+end
